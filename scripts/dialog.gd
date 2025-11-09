735155806b98929212b8ca84c44b9e6ac4466548
Jack-of-all-trades-04 <novanagung08@gmail.com>
2025-11-10 01:15:36 +0700

    Add GUI, MainMenu, Tutorial, ModifyDialog. Tapi masih belum bagus semua kayaknya

--- scripts/dialog.gd
+++ scripts/dialog.gd
@@ -1,42 +1,105 @@
-@tool
 extends Control
 
-# Node Variable
-@onready var Anim = $AnimationPlayer
-@onready var NodeText = $Text
+# Variabel Lokal
+@onready var Anim: AnimationPlayer = $AnimationPlayer
+@onready var NodeText: RichTextLabel = $Text
+var temp : String
 
-# Editor Variable
-@export_multiline var Text : String
-@export var Play : bool
+# Editor variables
+@export_multiline var Text: String = ""
 @export_category("Animation")
 @export_group("Typewriter Animation")
-@export var TypewriterAnimation : bool
-@export var Duration_Second : float
+@export var TypewriterAnimation: bool = false
+@export var Type_Duration: float = 2.0
+@export var Play_Type: bool = false
+@export_group("Fade Animation")
+@export var Fadein: bool = false
+@export var Fadein_Duration: float = 1.0
+@export var Fadeout: bool = false
+@export var Fadeout_Duration: float = 1.0
+@export var Play_Fade: bool = false
 
-
-func _draw() -> void:
-	if TypewriterAnimation :
-		var animation = Anim.get_animation("Typewriter")
-		if animation.get_track_count() == 1 : animation.remove_track(0)
-		var track = animation.add_track(Animation.TYPE_VALUE)
-		var text_length = NodeText.get_total_character_count()
-		animation.length = Duration_Second
-		animation.set_loop_mode(false)
-		animation.track_set_path(track, "Text:visible_characters")
-		animation.track_insert_key(track, 0.0, 0)
-		animation.track_insert_key(track, Duration_Second, text_length)
-		Anim.play("Typewriter")
-	else :
+func _ready() -> void:
+	if Engine.is_editor_hint():
 		NodeText.text = Text
 		NodeText.visible_characters = -1
+	else:
+		NodeText.text = ""
+	_create_animations()
+
+func _create_animations():
+	if not Anim.has_animation("Typewriter"):
+		Anim.add_animation("Typewriter")
+	if not Anim.has_animation("FadeIn"):
+		Anim.add_animation("FadeIn")
+	if not Anim.has_animation("FadeOut"):
+		Anim.add_animation("FadeOut")
+	_setup_typewriter_anim()
+	_setup_fadein_anim()
+	_setup_fadeout_anim()
+
+func setText(text:String) :
+	NodeText.text = text
+	NodeText.visible_characters = 0
+
+func _setup_typewriter_anim():
+	var anim = Anim.get_animation("Typewriter")
+	anim.clear()
+	var text_length = NodeText.get_total_character_count()
+	anim.length = Type_Duration
+	anim.loop_mode = Animation.LOOP_NONE
+	var track = anim.add_track(Animation.TYPE_VALUE)
+	anim.track_set_path(track, "Text:visible_characters")
+	anim.track_insert_key(track, 0.0, 0)
+	anim.track_insert_key(track, Type_Duration, text_length)
+
+func _setup_fadein_anim():
+	var anim = Anim.get_animation("FadeIn")
+	temp = NodeText.text
+	NodeText.text = ""
+	anim.clear()
+	anim.length = Fadein_Duration
+	anim.loop_mode = Animation.LOOP_NONE
+	var track = anim.add_track(Animation.TYPE_VALUE)
+	anim.track_set_path(track, "Text:self_modulate")
+	anim.track_insert_key(track, 0.0, Color(1, 1, 1, 0))
+	anim.track_insert_key(track, Fadein_Duration, Color(1, 1, 1, 1))
+
+func _setup_fadeout_anim():
+	var anim = Anim.get_animation("FadeOut")
+	anim.clear()
+	anim.length = Fadeout_Duration
+	anim.loop_mode = Animation.LOOP_NONE
+	var track = anim.add_track(Animation.TYPE_VALUE)
+	anim.track_set_path(track, "Text:self_modulate")
+	anim.track_insert_key(track, 0.0, Color(1, 1, 1, 1))
+	anim.track_insert_key(track, Fadeout_Duration, Color(1, 1, 1, 0))
 
 func _process(delta: float) -> void:
-	if Play && TypewriterAnimation:
-		Duration_Second = Duration_Second
-		Anim.play("Typewriter")
-	else :
+	if Play_Type and TypewriterAnimation:
+		Play_Type = false
 		NodeText.text = Text
-		NodeText.visible_characters = -1
+		_setup_typewriter_anim()
+		Anim.play("Typewriter")
+
+	elif Play_Fade:
+		Play_Fade = false
+		if Fadein:
+			_setup_fadein_anim()
+			Anim.play("FadeIn")
+		elif Fadeout:
+			_setup_fadeout_anim()
+			Anim.play("FadeOut")
 
 func _on_animation_player_animation_finished(anim_name: StringName) -> void:
-	Play = false
+	match anim_name:
+		"FadeIn":
+			if TypewriterAnimation:
+				NodeText.text = temp
+				temp = ""
+				Anim.play("Typewriter")
+			elif Fadeout :
+				Anim.play("FadeOut")
+		"Typewriter":
+			if Fadeout:
+				Anim.play("FadeOut")
